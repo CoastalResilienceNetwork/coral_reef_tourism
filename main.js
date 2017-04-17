7a487f6c305962408e15eca111fdc122bf4ada30
Jacob Lesser <jacob@prusikmapping.com>
2017-04-17 06:20:56 -0700

    Add bar graph

--- main.js
+++ main.js
@@ -33,13 +33,16 @@ define([
 			resizable: false,
 			width: 425,
 			size: 'custom',
-			chart: {},
+			chart: {
+				margin: {top: 20, right: 20, bottom: 30, left: 75},
+			    width: 410 - 40,
+			    height: 310 - 50
+			},
 
 			initialize: function(frameworkParameters) {
 				declare.safeMixin(this, frameworkParameters);
 				this.$el = $(this.container);
 				this.stats = $.parseJSON(Stats);
-				console.log(this.stats);
 			},
 
 			bindEvents: function() {
@@ -52,19 +55,9 @@ define([
 
 				this.$el.find('#chosenRegion').on('change', function(e) {
 					//self.zoomToRegion(e.target.value);
-					var region = e.target.value.toUpperCase();
-
-					if (region === "GLOBAL") {
-						self.$el.find('.stats .header .region-label').html('the World');
-					} else {
-						self.$el.find('.stats .header .region-label').html(region);
-					}
-
-					self.$el.find('.stat.reef_value .number .value').html(self.addCommas(self.stats[region].reef_value));
-					self.$el.find('.stat.reef_area_tourism .number .value').html(self.addCommas(self.stats[region].reefs_area));
-					self.$el.find('.stat.reef_area_tourism .number .percentage').html((self.stats[region].reefs_used_percent * 100).toFixed(1));
-					
-
+					var region = e.target.value;
+					self.updateStats(region);
+					self.updateChartData(region);
 				});
 
 			},
@@ -97,8 +90,11 @@ define([
 			},
 
 			render: function() {
-				this.$el.html(_.template(template, {
 
+				var regions = Object.keys(this.stats).sort();
+
+				this.$el.html(_.template(template)({
+					regions: regions
                 }));
 
                 this.$el.find('#chosenRegion').chosen({
@@ -117,21 +113,186 @@ define([
 					});
 				});
 
+				this.updateStats('Global');
+
                 this.renderChart();
+
+                this.updateChartData('Global');
 
                 this.bindEvents();
 
                 this.$el.find(".stat-info span").tooltip();
 			},
 
+			updateStats: function(region) {
+				if (region === "Global") {
+					this.$el.find('.stats .header .region-label').html('the World');
+				} else {
+					this.$el.find('.stats .header .region-label').html(region);
+				}
+
+				this.$el.find('.stat.reef_value .number .value').html(this.addCommas(this.stats[region].reef_value));
+				this.$el.find('.stat.total_visitation .number .value').html(this.addCommas(this.stats[region].total_visitation_value));
+				this.$el.find('.stat.reef_highest_value .number .value').html(this.addCommas(this.stats[region].higest_value_reefs));
+				this.$el.find('.stat.reef_area .number .value').html(this.addCommas(this.stats[region].total_reef_area.toFixed(0)));
+				this.$el.find('.stat.reef_area_tourism .number .value').html(this.addCommas(this.stats[region].reefs_tourism_area.toFixed(0)));
+				this.$el.find('.stat.reef_area_tourism .number .percentage').html((this.stats[region].reefs_tourism_area_percent * 100).toFixed(0));
+			},
+
 			renderChart: function() {
-				console.log(this);
+				var self = this;
+
+				var margin = {
+					top: 20,
+					right: 20, 
+					bottom: 30,
+					left: 40
+				};
+			    var width = this.chart.width = 382 - margin.left - margin.right;
+			    var height = this.chart.height = 300 - margin.top - margin.bottom;
+
+			    var x = this.chart.x = d3.scale.ordinal().rangeRoundBands([0, width], 0.1);
+    			var y = this.chart.y = d3.scale.linear().range([height, 0]);
+
+    			var data = [
+					{x: "On Reef", y: this.stats.Global.onreef_value},
+					{x: "Adjacent Reef", y: this.stats.Global.adjacent_value}
+				];
+
 				this.chart.svg = d3.selectAll(".chart")
 	                .append("svg")
-	                    .attr("width", 350)
-	                    .attr("height", 235)
+	                    .attr("width", width + margin.left + margin.right)
+	                    .attr("height", height + margin.bottom + margin.top);
+
+	            var g = this.chart.svg.append("g")
+    				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    			x.domain(["On Reef", "Adjacent Reef"]);
+  				y.domain([0, d3.max(data, function(d) { return d.y; })]);
+
+  				var xAxis = d3.svg.axis().scale(x).orient("bottom");
+  				var yAxis = d3.svg.axis().scale(y).orient("left").ticks(6).tickFormat(function(d) {
+					return d / 1000000000;
+				});
+
+  				g.append("g")
+					.attr("class", "x axis")
+					.attr("transform", "translate(0," + height + ")")
+					.call(xAxis);
+
+			    g.append("g")
+					.attr("class", "y axis")
+					.call(yAxis);
+
+  				g.selectAll(".bar")
+				    .data(data)
+				    .enter().append("rect")
+						.attr("class", "bar")
+						.attr("title", function(d) {
+							return parseInt(d.y).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+						})
+						.attr("x", function(d) { return x(d.x); })
+						.attr("y", function(d) { return y(d.y); })
+						.attr("width", x.rangeBand())
+						.attr("height", function(d) { return height - y(d.y); });
+
+/*
 	                .append("g")
-	                    .attr("transform", "translate(" + 20 + "," + 20 + ")");
+	                    .attr("transform", "translate(" + this.chart.margin.left + "," + this.chart.margin.right + ")");
+/*
+	            this.chart.x = d3.scale.ordinal()
+                    .domain(["On Reef", "Adjacent Reef"])
+                    .rangeRoundBands([0, this.chart.width], 0.15);
+
+                this.chart.y = d3.scale.linear()
+                    .range([this.chart.height, 0])
+                    .domain([0, 6000000000]);
+
+                this.chart.xAxis = d3.svg.axis()
+                    .scale(this.chart.x)
+                    .orient("bottom");
+
+                this.chart.yAxis = d3.svg.axis()
+                    .scale(this.chart.y)
+                    .orient("left").ticks(5);
+
+
+                // Add a chart background object that can be styled separately
+                /*this.chart.svg.append("rect")
+                    .attr("class", "chart-area")
+                    .attr("width", 300)
+                    .attr("height", 215)
+                    .attr("fill", "#f6f6f6");
+*/
+                // Add the xaxis
+ /*               this.chart.svg.append("g")
+                    .attr("opacity", 1)
+                    .attr("class", "xaxis")
+                    .attr("transform", "translate(0," + (this.chart.height - this.chart.margin.bottom) + ")")
+                    .call(this.chart.xAxis);
+
+                // Add the x-axis label
+                this.chart.svg.append("text")
+                    .attr("class", "xaxis-label")
+                    .attr("opacity", 0)
+                    .attr("text-anchor", "middle")
+                    .attr("transform", "translate(" + (175) + "," + (215) + ")")
+                    .text("Reefs used for Tourism");
+
+                // Add the y-axis label
+                this.chart.svg.append("text")
+                    .attr("class", "yaxis-label")
+                    .attr("transform", "rotate(-90)")
+                    .attr("y", 0 - 50)
+                    .attr("x", 0 - (115))
+                    .attr("text-anchor", "middle")
+                    .text("Reef Value");
+
+                this.chart.svg.append("g")
+                    .attr("class", "yaxis")
+                    .call(this.chart.yAxis);
+
+                var data = [
+					{x: "On Reef", y: this.stats.Global.onreef_value},
+					{x: "Adjacent Reef", y: this.stats.Global.adjacent_value}
+				];
+
+                this.chart.svg.selectAll(".bar")
+                    .data(data)
+                    .enter().append("rect")
+                    .attr("opacity", 1)
+                    .attr("class", "bar info-tooltip")
+                    .transition().duration(1200).ease("sin-in-out")
+                    .attr("x", function(d) { return self.chart.x(d.x); })
+                    .attr("y", function(d) { return self.chart.y(d.y); })
+                    .attr("width", 30)
+                    .attr("height", function(d) { return 215 - self.chart.y(d.y); })
+                    .attr("title", function(d) {
+                        return parseInt(d.y).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                    });
+
+                    $('.chart rect').tooltip();
+
+*/
+			},
+
+			updateChartData: function(region) {
+				var self = this;
+				var data = [
+					{x: "On Reef", y: this.stats[region].onreef_value},
+					{x: "Adjacent Reef", y: this.stats[region].adjacent_value}
+				];
+
+				this.chart.svg.selectAll(".bar")
+				    .data(data)
+				    .transition().duration(1200).ease("sin-in-out")
+			      	.attr("class", "bar")
+					.attr("title", function(d) {
+					    return parseInt(d.y).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+					})
+					.attr("y", function(d) { console.log(self.chart.y);return self.chart.y(d.y); })
+					.attr("height", function(d) { return self.chart.height - self.chart.y(d.y); });
+
 
 			},
 
